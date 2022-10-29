ac234a1d57b559b21df7ec366f003a3720b39cc5
SemSlipchenko <semslipchenko@gmail.com>
2022-10-29 17:14:56 +0300

    adding base css and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,4 @@
+import FeedBack from './Feedback/FeedBack';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <FeedBack />;
 };
---
+++ src/components/Feedback/FeedBack.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FeedBack extends React.Component {
+  render() {
+    return <h1>Please leave feedback</h1>;
+  }
+}
+
+export default FeedBack;
